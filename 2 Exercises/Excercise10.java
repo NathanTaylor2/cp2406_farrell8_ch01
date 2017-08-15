d62f692513289592d0f01b9dc614a2d31be4bfd2
NathanTaylor2 <nathan.taylor2@my.jcu.edu.au>
2017-08-15 17:28:21 +1000

    My Prac 1 Solution

---
+++ 2 Exercises/Excercise10.java
@@ -0,0 +1,9 @@
+public class Excercise10 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hey now, you're an all-star, get your game on, go play");
+        System.out.println("Hey now, you're a rock star, get the show on, get paid");
+        System.out.println("And all that glitters is gold");
+        System.out.println("Only shooting stars break the mold…");
+}
+}
---
+++ 2 Exercises/Exercise6.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Exercise6 {
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "What killed the dinosaurs, the Ice Age!");
+    }
+}
